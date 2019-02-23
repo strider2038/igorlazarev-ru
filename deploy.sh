cf1f14915599f9a4ede7423f190d7442fb474385
Igor Lazarev <strider2038@yandex.ru>
2019-02-23 14:07:01 +0300

    travis ci deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,11 @@
 
 set -e
 
-scp -P "$SERVER_PORT" "$SERVER_USER@$SERVER_IP_ADDRESS":~/igorlazerev-ru/tmp/site.zip site.zip
+scp -P "$SERVER_PORT" -i ./deploy_key site.zip "$SERVER_USER@$SERVER_IP_ADDRESS":~/igorlazarev.ru/tmp/site.zip
+
+ssh -p "$SERVER_PORT" -i ./deploy_key -q "$SERVER_USER@$SERVER_IP_ADDRESS" <<EOF
+unzip ~/igorlazarev.ru/tmp/site.zip -d ~/igorlazarev.ru/tmp
+rm -rf ~/igorlazarev.ru/public
+mv ~/igorlazarev.ru/tmp/_site ~/igorlazarev.ru/public
+rm -rf ~/igorlazarev.ru/tmp/*
+EOF
