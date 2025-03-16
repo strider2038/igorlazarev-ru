b8badf0b686689d22ae3bc721c1def6de46a911c
Igor Lazarev <i.lazarev@istock.info>
2025-03-16 18:56:54 +0300

    nullable or not nullable

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./public")))
-	log.Println("running on http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
+	log.Println("running on http://localhost:1313")
+	err := http.ListenAndServe(":1313", nil)
 	if err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Fatal(err)
 	}
